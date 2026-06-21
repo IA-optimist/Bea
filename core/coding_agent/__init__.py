6e7ffd8cfb23d318488aa6295805d0d6be0221c5
Maxence Londot <londot.maxence3@gmail.com>
2026-06-21 14:01:31 +0200

    Add coding agent worktree loop

--- core/coding_agent/__init__.py
+++ core/coding_agent/__init__.py
@@ -8,10 +8,21 @@ from core.coding_agent.quality_gate import (
 )
 from core.coding_agent.repo_map import RepoMap, build_repo_map
 from core.coding_agent.swe_lite import SWELiteReport, run_swe_lite_v1
+from core.coding_agent.worktree_loop import (
+    CodingAgentRunner,
+    CodingRun,
+    CommandExecution,
+    MissionInput,
+    select_targeted_tests,
+)
 
 __all__ = [
+    "CodingAgentRunner",
+    "CodingRun",
+    "CommandExecution",
     "FailureMemory",
     "FailureRecord",
+    "MissionInput",
     "QualityGateCommand",
     "QualityGatePlan",
     "RepoMap",
@@ -19,4 +30,5 @@ __all__ = [
     "build_quality_gate_plan",
     "build_repo_map",
     "run_swe_lite_v1",
+    "select_targeted_tests",
 ]
